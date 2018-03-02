3ef1cf47cf68b5d68a0d047b9f957aac3cbcad3b
joncady <joncady@uw.edu>
2018-03-02 13:55:33 -0800

    ui updated

--- ui.R
+++ ui.R
@@ -8,28 +8,37 @@ shinyUI(
     
     # Would like to implement navbarpage, would provide for easy navigation
     
-    navbarPage("Business",
-                      tabPanel("Business Search", 
-                        titlePanel("Yelp Business"),
-                             
-                        sidebarLayout(
-                          sidebarPanel(
-                          textInput("search_input", "Type your search here"),
-                          textInput("location_input", "Type your location here"),
-                          actionButton("search_button", label = "", icon = shiny::icon("search"))
-                         
-                        ),
-                                 
-                        mainPanel(
-                          DT::dataTableOutput("businesses") 
-                               
-                        )
-                                 
-                      )
-                               
-                      ),
-                      tabPanel("Component 2"),
-                      tabPanel("Component 3")
+    navbarPage("Yelp Business App",
+      tabPanel("Business Search", 
+        titlePanel("Business Search from Location"),
+             
+        sidebarLayout(
+          sidebarPanel(
+          textInput("search_input", "Type your search here"),
+          textInput("location_input", "Type your location here"),
+          actionButton("search_button", label = "", icon = shiny::icon("search"))
+         
+        ),
+                 
+        mainPanel(
+          DT::dataTableOutput("businesses") 
+               
+        )
+                 
+      )
+               
+      ),
+      
+      # panel for plotting information related to business
+      tabPanel("Plotting Stuff?",
+        "hi"         
+      ),
+      
+      
+      tabPanel("Review Search",
+        textInput("review_input", "Enter a business to find reviews on"),     
+        actionButton("review_button", label = "", icon = shiny::icon("search"))
+      )
     )
   )
 )
