1919e7de9220944d356198f53a28fe191228cf27
joncady <joncady@uw.edu>
2018-03-06 12:40:58 -0800

    fixed table

--- server.R
+++ server.R
@@ -6,13 +6,15 @@ library(ggplot2)
 library(maps)
 library(mapproj)
 library(leaflet)
+library(httr)
+library(jsonlite)
 
 source("key.R")
 
 function(input, output, session){
   observeEvent(input$search_button, {
     base_yelp_url <- "https://api.yelp.com/v3/"
-    path = "businesses/search"
+    path = "businesses/search" 
     query.params = list(term = input$search_input, location = input$location_input, limit = 50)
     response <- GET(url = paste(base_yelp_url, path, sep = ""), query = query.params, add_headers('Authorization' = paste("bearer", yelp_api_key)), content_type_json())
     body <- content(response, "text")
@@ -23,6 +25,16 @@ function(input, output, session){
     compress <- flatten(business_data[[1]]) %>% select(-id, -is_closed, -categories, -location.display_address, -categories, -transactions, -coordinates.latitude, -coordinates.longitude, -distance, -display_phone)
     compress$image_url <- paste("<img src='", compress$image_url, "' height = '60'</img>", sep = "")
     compress$url <- paste0("<a href='", compress$url, "' class = 'button'>Website</a>")
+    
+    # combine addresses to make clean looking address column
+    compress$address <- paste0(compress$location.address1, "," , compress$location.city, ", ", compress$location.state, ", ", compress$location.zip_code, ", ", compress$location.country) 
+                             
+    compress <- select(compress,-location.address1, -location.address2, -location.city, -location.state, -location.zip_code, -location.address3, -location.country)
+  
+    
+    # cleaning up coumn titles:
+    colnames(compress) <- c("Name", "Image", "Yelp Link", "Review Count", "Rating", "Price", "Phone", "Address")
+    
     output$businesses <- renderDataTable(DT::datatable(compress, escape = FALSE, selection = "none"))
   
   })
