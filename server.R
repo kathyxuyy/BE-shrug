c4425b9b19d501ce8fa78f0f653b80daf5671f71
Jit Li <JitPossible@Jits-MacBook-Air.local>
2018-03-06 16:06:07 -0800

    Added action button and included comments for location analysis.

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-  # server.R file
+# server.R file
 
 library(dplyr)
 library(DT)
@@ -29,15 +29,15 @@ function(input, output, session){
     
     # combine addresses to make clean looking address column
     compress$address <- paste0(compress$location.address1, "," , compress$location.city, ", ", compress$location.state, ", ", compress$location.zip_code, ", ", compress$location.country) 
-                             
+    
     compress <- select(compress,-location.address1, -location.address2, -location.city, -location.state, -location.zip_code, -location.address3, -location.country)
-  
+    
     
     # cleaning up coumn titles:
     colnames(compress) <- c("Name", "Image", "Yelp Link", "Review Count", "Rating", "Price", "Phone", "Address")
     
     output$businesses <- renderDataTable(DT::datatable(compress, escape = FALSE, selection = "none"))
-  
+    
   })
   
   map <- leaflet() %>% addTiles() %>% setView(-101.204687, 40.607628, zoom = 3)
@@ -56,18 +56,18 @@ function(input, output, session){
     center <- region[[1]]
     
     business_frame <- flatten(specific_data[[1]])
-
+    
     if (input$business_filter != "") {
-        business_frame <- filter(business_frame, price == input$business_filter)
+      business_frame <- filter(business_frame, price == input$business_filter)
     }
     if (nrow(business_frame) == 0) {
       view_city <- geocode(input$location_box)
       output$myMap <- renderLeaflet(map %>% setView(view_city[[1]], view_city[[2]], zoom = 13))
     } else {
       output$myMap <- renderLeaflet(map %>% 
-                                    setView(center[[1]],center[[2]], zoom = 13) %>% 
-                                    addMarkers(lng = business_frame$coordinates.longitude, 
-                                              lat = business_frame$coordinates.latitude, icon=greenLeafIcon, label = business_frame$name))
+                                      setView(center[[1]],center[[2]], zoom = 13) %>% 
+                                      addMarkers(lng = business_frame$coordinates.longitude, 
+                                                 lat = business_frame$coordinates.latitude, icon=greenLeafIcon, label = business_frame$name))
     }  
     
     getColor <- function(business_frame) {
@@ -89,7 +89,7 @@ function(input, output, session){
       shadowWidth = 50, shadowHeight = 64,
       shadowAnchorX = 4, shadowAnchorY = 62
     )
-  
+    
   })
   
   observeEvent(input$compare, {
@@ -132,35 +132,55 @@ function(input, output, session){
     
   })
   
-  output$analytics <- renderPlot({
-
-    
-    requestData <- function(n) {
-      path = "businesses/search" 
-      query.params = list(term = "food", location = input$search_location, limit=50, offset=50*n-50)
-      suppressWarnings(response <- GET(url = paste(base_yelp_url, path, sep = ""), query = query.params, add_headers('Authorization' = paste("bearer", yelp_api_key)), content_type_json()))
-      body <- content(response, "text")
-      data <- fromJSON(body)
-      compressed <- flatten(data[[1]])
-      return (compressed)
-    }
-    
-    business.info <- data.frame()
-    for (i in 1:20) {
-      data <- requestData(i)
-      business.info <- rbind(business.info, data)
-    }
-    business.categories <- data.frame();
-    for (i in 1:1000) {
-      x <- business.info$categories[[i]]
-      business.categories <- rbind(business.categories, x)
-    }
-    business.categories <- business.categories %>%
-      group_by(title) %>%
-      summarize(count = n())
-    business.categories<- business.categories[with(business.categories,order(-count)),]
-    business.categories <- business.categories[1:6,]
-    ggplot(business.categories, aes(x = reorder(title, -count), y = count)) + geom_bar(stat = "identity") + labs(x="Categories", y="Count") +
-      ggtitle(paste0("Top 6 Categories in ", input$search_location)) + theme(plot.title = element_text(size = 30, face = "bold", hjust= 0.5 ))
+  
+  
+  observeEvent(input$analysis_button, {
+    output$analytics <- renderPlot({
+      
+      # This function requests business information from the YELP api
+      requestData <- function(n) {
+        path = "businesses/search" 
+        query.params = list(term = "food", location = input$search_location, limit=50, offset=50*n-50)
+        response <- GET(url = paste(base_yelp_url, path, sep = ""), query = query.params, add_headers('Authorization' = paste("bearer", yelp_api_key)), content_type_json())
+        body <- content(response, "text")
+        data <- fromJSON(body)
+        suppressWarnings(compressed <- flatten(data[[1]]))
+        return (compressed)
+      }
+      
+      # Creates a new data frame to store Yelp business information requested from the API
+      business.info <- data.frame()
+      
+      # This loop iterates over 20 times to obtain information from the YELP API(because YELP API only returns a maximum of 50 rows 
+      # per GET request and up to a 1000 rows as a whole)
+      # !! This may take a while due to the fact that 20 GET requests has to be made
+      for (i in 1:20) {
+        data <- requestData(i)
+        business.info <- rbind(business.info, data)
+      }
+      
+      # Creates a new data frame to store information on various categories of businesses
+      business.categories <- data.frame();
+      
+      # This loops takes information from the "Category" column of the business dataframe(business.info) and transfers the information into a new Dataframe
+      for (i in 1:nrow(business.info)) {
+        data <- business.info$categories[[i]]
+        business.categories <- rbind(business.categories, data)
+      }
+      
+      # This alters the business.category dataset by grouping together similar Categories and counting the amounts of those Categories
+      business.categories <- business.categories %>%
+        group_by(title) %>%
+        summarize(count = n())
+      
+      # This alters the business.category dataset by only keeping the top 6 most occuring Categories
+      business.categories<- business.categories[with(business.categories,order(-count)),]
+      business.categories <- business.categories[1:6,]
+      
+      # This plots a barplot to display the top 6 Categories for the various locations chosen.
+      ggplot(business.categories, aes(x = reorder(title, -count), y = count)) + geom_bar(stat = "identity") + labs(x="Categories", y="Count") +
+        ggtitle(paste0("Top 6 Categories in ", input$search_location)) + theme(plot.title = element_text(size = 30, face = "bold", hjust= 0.5 ))
+    })
   })
+  
 }
--- ui.R
+++ ui.R
@@ -6,107 +6,108 @@ library(DT)
 
 shinyUI(
   fluidPage(theme = "bootstrap.css", class = "sure",
-    includeCSS("style.css"),
-      navbarPage("Yelp Business", id = "no", inverse = TRUE,
-               tabPanel("Business Search",
-                        sidebarLayout(
-                          
-                          sidebarPanel(
-                            textInput("search_input", "Type your search here"),
-                            textInput("location_input", "Type your location here"),
-                            actionButton("search_button", label = "", icon = shiny::icon("search"))
-                            
-                          ),
-                          mainPanel(id = "yes",
-                            dataTableOutput("businesses")
-                          )
-                        )
-               ),  
-               
-               tabPanel("Location Search",
-                        sidebarLayout(
-                          sidebarPanel(
-                            textInput("search_box", "Type your business here"),
-                            textInput("location_box", "Type your location here"),
-                            
-                            #filters for the map: price, 
-                            radioButtons("business_filter", label = "Prices", choices = list("No Preference" = "", "$", "$$", "$$$", "$$$$")),
-                            actionButton("location_button", label = "", icon = shiny::icon("search"))
-                          ),
-                          
-                          
-                          mainPanel(
-                            leafletOutput('myMap', height = "800")
-                          )
-                        )
-               ),
-               
-               tabPanel("Business Comparison",
-                        
-                        # inputs at side of page       
-                        
-                        # sidebarLayout(
-                        #   sidebarPanel(
-                        #     textInput("name1", "Type business' name here"),
-                        #     textInput("name2", "Type business' name here"),
-                        #     textInput("locationlocation", "Type your location here"),
-                        #     actionButton("compare", label = "", icon = shiny::icon("search"))
-                        #   ),
-                        #   mainPanel(
-                        #     column(12,
-                        #     dataTableOutput("test")
-                        #     ),
-                        #     column(12,
-                        #             dataTableOutput("review")
-                        #     )
-                        #   )
-                        # )
-                        
-                        # inputs at the top of page
-                        fluidRow(
-                          column(3,
-                                 textInput("name1", "Type business' name here")
-                          ),
-                          column(3,
-                                 textInput("locationlocation", "Type your location here")
-                          ),
-                          column(3,
-                                 textInput("name2", "Type business' name here")
-                          ),
-                          column(1,
-                                 actionButton("compare", label = "", icon = shiny::icon("search"))
-                          )
-                        ),
-                        
-                        fluidRow(
-                          column(6, 
-                                 h2(textOutput("bn1")),
-                                 htmlOutput("bi1"),
-                                 textOutput("phone"),
-                                 textOutput("bp1"),
-                                 textOutput("address"),
-                                 textOutput("ba1p1"),
-                                 textOutput("ba1p3"),
-                                 textOutput("ba1p2"),
-                                 h3(textOutput("star"))
-                                 ),
-                          column(6, dataTableOutput("test"))
-                        )
-               ),
-               
-               
-               tabPanel("Location Analytics",
-                        sidebarLayout(
-                          sidebarPanel(
-                            textInput("search_location", "Enter a location:", value = "Seattle")
-                          ),
-                          mainPanel(
-                            plotOutput("analytics")
-                          )
-                          
-                        )         
-                        
-               )
-    )
+            includeCSS("style.css"),
+            navbarPage("Yelp Business", id = "no", inverse = TRUE,
+                       tabPanel("Business Search",
+                                sidebarLayout(
+                                  
+                                  sidebarPanel(
+                                    textInput("search_input", "Type your search here"),
+                                    textInput("location_input", "Type your location here"),
+                                    actionButton("search_button", label = "", icon = shiny::icon("search"))
+                                    
+                                  ),
+                                  mainPanel(id = "yes",
+                                            dataTableOutput("businesses")
+                                  )
+                                )
+                       ),  
+                       
+                       tabPanel("Location Search",
+                                sidebarLayout(
+                                  sidebarPanel(
+                                    textInput("search_box", "Type your business here"),
+                                    textInput("location_box", "Type your location here"),
+                                    
+                                    #filters for the map: price, 
+                                    radioButtons("business_filter", label = "Prices", choices = list("No Preference" = "", "$", "$$", "$$$", "$$$$")),
+                                    actionButton("location_button", label = "", icon = shiny::icon("search"))
+                                  ),
+                                  
+                                  
+                                  mainPanel(
+                                    leafletOutput('myMap', height = "800")
+                                  )
+                                )
+                       ),
+                       
+                       tabPanel("Business Comparison",
+                                
+                                # inputs at side of page       
+                                
+                                # sidebarLayout(
+                                #   sidebarPanel(
+                                #     textInput("name1", "Type business' name here"),
+                                #     textInput("name2", "Type business' name here"),
+                                #     textInput("locationlocation", "Type your location here"),
+                                #     actionButton("compare", label = "", icon = shiny::icon("search"))
+                                #   ),
+                                #   mainPanel(
+                                #     column(12,
+                                #     dataTableOutput("test")
+                                #     ),
+                                #     column(12,
+                                #             dataTableOutput("review")
+                                #     )
+                                #   )
+                                # )
+                                
+                                # inputs at the top of page
+                                fluidRow(
+                                  column(3,
+                                         textInput("name1", "Type business' name here")
+                                  ),
+                                  column(3,
+                                         textInput("locationlocation", "Type your location here")
+                                  ),
+                                  column(3,
+                                         textInput("name2", "Type business' name here")
+                                  ),
+                                  column(1,
+                                         actionButton("compare", label = "", icon = shiny::icon("search"))
+                                  )
+                                ),
+                                
+                                fluidRow(
+                                  column(6, 
+                                         h2(textOutput("bn1")),
+                                         htmlOutput("bi1"),
+                                         textOutput("phone"),
+                                         textOutput("bp1"),
+                                         textOutput("address"),
+                                         textOutput("ba1p1"),
+                                         textOutput("ba1p3"),
+                                         textOutput("ba1p2"),
+                                         h3(textOutput("star"))
+                                  ),
+                                  column(6, dataTableOutput("test"))
+                                )
+                       ),
+                       
+                       
+                       tabPanel("Location Analytics",
+                                sidebarLayout(
+                                  sidebarPanel(
+                                    textInput("search_location", "Enter a location:", value = "Seattle"),
+                                    actionButton("analysis_button", label = "", icon = shiny::icon("search"))
+                                  ),
+                                  mainPanel(
+                                    plotOutput("analytics")
+                                  )
+                                  
+                                )         
+                                
+                       )
+            )
   )
 )
